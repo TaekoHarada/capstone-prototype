2998d308bc90f370099657e35bd2a92cb6c435f3
rishabprakash <141350228+rishabprakash@users.noreply.github.com>
2024-10-30 23:08:04 -0600

    Update page.js

--- src/app/dashboard/drawDesign/page.js
+++ src/app/dashboard/drawDesign/page.js
@@ -16,18 +16,16 @@ const DrawDesignPage = () => {
     setIsCanvasVisible(true);
   };
 
- return (
-  <div>
-    <h1>Draw Design Feature</h1> {/* Add this line */}
-    <button onClick={() => setIsCanvasVisible(!isCanvasVisible)}>Draw Design</button>
-    {isCanvasVisible && (
-      <div>
-        <ControlPanel onDimensionsSubmit={handleStartDrawing} />
-        <RoomCanvas roomDimensions={roomDimensions} />
-      </div>
-    )}
-  </div>
-);
-
+  return (
+    <div>
+      <button onClick={() => setIsCanvasVisible(!isCanvasVisible)}>Draw Design</button>
+      {isCanvasVisible && (
+        <div>
+          <ControlPanel onDimensionsSubmit={handleStartDrawing} />
+          <RoomCanvas roomDimensions={roomDimensions} />
+        </div>
+      )}
+    </div>
+  );
 };
 export default DrawDesignPage;
