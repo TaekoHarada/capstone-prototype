39ddef88fe91ab24215a2f94d44f27c0ee4ac61c
rishabprakash <141350228+rishabprakash@users.noreply.github.com>
2024-11-04 23:01:06 -0700

    created the functionality to show a canvvas based on entered width and length of room

--- src/app/dashboard/drawDesign/RoomCanvas.js
+++ src/app/dashboard/drawDesign/RoomCanvas.js
@@ -1,25 +1,26 @@
 // RoomCanvas.js
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { furnitureData } from './furnitureData';
 
 const RoomCanvas = ({ roomDimensions }) => {
+  const canvasRef = useRef(null);
   const { length, width } = roomDimensions;
 
-  // Dynamically import Konva components on the client side
-  let Stage, Layer;
-  if (typeof window !== "undefined") {
-    const Konva = require('react-konva');
-    Stage = Konva.Stage;
-    Layer = Konva.Layer;
-  }
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    // Set canvas dimensions based on room dimensions
+    canvas.width = length * 10;
+    canvas.height = width * 10;
+
+    // Fill the background color
+    context.fillStyle = 'gray';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }, [length, width]);
 
   return (
-    Stage && Layer ? (
-      <Stage width={length * 10} height={width * 10}>
-        <Layer>
-          {/* Furniture items will be added here */}
-        </Layer>
-      </Stage>
-    ) : null
+    <canvas ref={canvasRef} style={{ border: '1px solid black' }} />
   );
 };
 
