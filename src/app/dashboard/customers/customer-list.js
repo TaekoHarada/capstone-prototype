c0a539b1610f2ee4208410ff34e643fbafceb245
Guneet <guneet.jassi@edu.sait.ca>
2024-11-05 21:59:01 -0700

    overall final

--- src/app/dashboard/customers/customer-list.js
+++ src/app/dashboard/customers/customer-list.js
@@ -14,13 +14,6 @@ const CustomerList = () => {
     const fetchCustomers = async () => {
       try {
         const data = await Customer.findAll();
-<<<<<<< HEAD
-
-        //sort with the 1st name//
-        // Sort customers alphabetically by firstname
-        
-=======
->>>>>>> 6bb7e8810fe07fe550c1ccfbbe05a6ec7207fb16
         const sortedData = data.sort((a, b) => a.firstname.localeCompare(b.firstname));
         setCustomers(sortedData);
       } catch (error) {
@@ -74,29 +67,6 @@ const CustomerList = () => {
           </Link>
         </div>
       </div>
-<<<<<<< HEAD
-      {error && <div className="text-red-500">{error}</div>}
-      <table className="hidden min-w-full text-gray-900 md:table">
-        <thead className="bg-gray-100 text-left text-sm font-normal">
-          <tr>
-            <th scope="col" className="px-3 py-3 font-medium">Customer ID</th>
-            <th scope="col" className="px-3 py-3 font-medium">First Name</th>
-            <th scope="col" className="px-3 py-3 font-medium">Last Name</th>
-            <th scope="col" className="px-3 py-3 font-medium">Email</th>
-            <th scope="col" className="px-3 py-3 font-medium">Phone</th>
-            <th scope="col" className="px-3 py-3 font-medium">Address</th>
-            <th scope="col" className="px-4 py-3 font-medium">Note</th>
-            <th scope="col" className="px-4 py-3 font-medium"></th>
-            <th scope="col" className="px-4 py-3 font-medium"></th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-900 text-gray-900">
-          {customers.map((customer) => (
-            <CustomerInfo key={customer.id} customer={customer} />
-          ))}
-        </tbody>
-      </table>
-=======
       {error && <div className="text-red-500 dark:text-red-400">{error}</div>}
       <div className="overflow-x-auto">
         <table className="min-w-full text-gray-900 dark:text-white bg-white dark:bg-gray-800">
@@ -120,7 +90,6 @@ const CustomerList = () => {
           </tbody>
         </table>
       </div>
->>>>>>> 6bb7e8810fe07fe550c1ccfbbe05a6ec7207fb16
     </div>
   );
 };
